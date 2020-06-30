13940ad7fea086aa4a0ac0617faf25c1bd3662a9
mifi6 <46782831+mifi6@users.noreply.github.com>
2020-06-30 20:24:17 +0900

    Created listAll and search functions

--- web/index.php
+++ web/index.php
@@ -1,5 +1,30 @@
 <?php
-// require('/db_conf.php');
-phpinfo();
+    require('./SetDB.php');
+    require('./DBControl.php');
 ?>
 
+<?php
+    // phpinfo();
+    $sDB = new SetDB();
+    $connection = $sDB->getConnectionStatus();
+
+    if($connection) {
+        $control = new DBControl($sDB->getPDO());
+
+        $result = $control->listAll('books');
+        foreach ( $result as $ele ) {
+            echo $ele;
+            echo '<br>';
+        }
+
+        $result = $control->search('hey');
+        echo '<br>' . '検索結果' . '<br>';
+        foreach ( $result as $ele ) {
+            echo $ele;
+            echo '<br>';
+        }
+
+    } else {
+        echo 'Failed to connect to DB';
+    }
+?>
